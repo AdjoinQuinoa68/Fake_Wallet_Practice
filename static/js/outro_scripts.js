4b3b36ec8df586e34be9947c1326d74ef47d00a1
Joaquín Díaz <diaz.adasme.joaquin@gmail.com>
2026-06-20 13:27:33 -0400

    Added user prototype

--- static/js/outro_scripts.js
+++ static/js/outro_scripts.js
@@ -2,12 +2,39 @@ function User(username) {
   this.username = username;
   this.money = 0;
 
-  this.deposit = function(amount) {
-    this.money += amount;
-  }
-  this.transfer = function(amount) {
-    this.money -= amount;
-  }
+  this.deposit = function(depositAmount) {
+    let amount = depositAmount.parseInt();
+    if (amount > 0) {
+      this.money += amount;
+      return 0
+    } else if (amount.isNan()) {
+      console.log("NaN (deposit)");
+      return 3;
+    } else if (amount < 0) {
+      console.log("Value can't be negative (deposit)");
+      return 2;
+    } else {
+      console.log("Unknown Error (deposit)");
+      return 1;
+    }
+  };
+
+  this.transfer = function (transferAmount) {
+    let amount = depositAmount.parseInt();
+    if (amount > 0) {
+      this.money -= amount;
+      return 0
+    } else if (amount.isNan()) {
+      console.log("NaN (transfer)");
+      return 3;
+    } else if (amount < 0) {
+      console.log("Value can't be negative (transfer)");
+      return 2;
+    } else {
+      console.log("Unknown Error (transfer)");
+      return 1;
+    }
+  };
 };
 
 function setUsername() {
