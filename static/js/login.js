a5d889090a11aecca5db21fd5341675794949fea
Joaquín Díaz <diaz.adasme.joaquin@gmail.com>
2026-06-23 12:00:23 -0400

    Refactor userList into a better data structure

--- static/js/login.js
+++ static/js/login.js
@@ -2,83 +2,50 @@
 $("#usernameFormButton").on("click", handleLogin);
 
 class User {
-  constructor(username, money = 0) {
-    this.username = username;
+  constructor(money = 0) {
     this.money = money;
     this.transactions = [];
   }
 
   depositMoney(depositAmount) {
-    let amount = depositAmount.parseInt();
-
-    if (amount > 0) {
-      this.money += amount;
-      return 0
-    } else if (amount.isNan()) {
-      console.log("NaN (deposit)");
-      return 3;
-    } else if (amount < 0) {
-      console.log("Value can't be negative (deposit)");
-      return 2;
-    } else {
-      console.log("Unknown Error (deposit)");
-      return 1;
-    }
+    this.money += depositAmount;
   }
 
   transferMoney(transferAmount) {
-    let amount = depositAmount.parseInt();
-
-    if (amount > 0) {
-      this.money -= amount;
-      return 0
-    } else if (amount.isNan()) {
-      console.log("NaN (transfer)");
-      return 3;
-    } else if (amount < 0) {
-      console.log("Value can't be negative (transfer)");
-      return 2;
+    if (transferAmount > this.money) {
+      console.log("Not enough funds");
     } else {
-      console.log("Unknown Error (transfer)");
-      return 1;
+      this.money -= transferAmount;
     }
   }
 };
 
-// If no userList exist: create dummy user and set userList.
-// Should only need to run the very first time a user visits the site.
+// Initializes userList if no user exists yet.
 $(document).ready(() => {
   if (!(localStorage.getItem("userList"))) {
-    const userList = JSON.stringify([new User("dummy", 1337), new User("Joaquin", 20000)]);
-    localStorage.setItem("userList", userList);
+    const initialUserList = JSON.stringify({"dummy": new User(1337), "Joaquin": new User(20000)});
+    localStorage.setItem("userList", initialUserList);
   }
 });
-
 
 function handleLogin() {
   const username = $("#usernameInputField").val();
   const userList = JSON.parse(localStorage.getItem("userList"));
-  console.log(userList);
-
-  let isUserInUserList = false;
-  userList.forEach(element => {
-    if (username == element.username) {
-      isUserInUserList = true;
-    }
-  });
 
   if (!username) {
     console.log("Username field empty");
-  } else if (isUserInUserList == true) {
+  } else if (username in userList) {
     console.log("Username valid. Loging in...");
     localStorage.setItem("currentUserUsername", username);
     window.location.assign("menu.html");
   } else {
     console.log("Username NOT in userList");
-    // prompt to create new user
-    // new user object gets pushed into the userList array
-    // and into localStorage
+    if (window.confirm("This user does not exist. Would you like to create one?")) {
+      const newUserList = Object.assign(userList, {[username]: new User(500)});
+      console.log(newUserList);
+      localStorage.setItem("userList", JSON.stringify(newUserList));
+      localStorage.setItem("currentUserUsername", username);
+      window.location.assign("menu.html");
+    }
   }
-  console.log(username);
-  console.log("is user in user list?:" + isUserInUserList);
 }
--- static/js/menu.js
+++ static/js/menu.js
@@ -1,16 +1,13 @@
 
 $("#userLogoffButton").on("click", userLogoff);
 
-currentUserUsername = localStorage.getItem("currentUserUsername")
+const currentUserUsername = localStorage.getItem("currentUserUsername");
+let userList = JSON.parse(localStorage.getItem("userList"));
 
 function userLogoff() {
   localStorage.setItem("currentUserUsername", "");
   window.location.assign("index.html");
 }
 
-function readCurrentUser(username) {
-  return JSON.parse(localStorage.getItem("userList")).filter((user => user.username == username))[0];
-}
-
 $("#menuTitle").text(`Bienvenido, ${currentUserUsername}!`);
-$("#currentBalance").text(`$ ${readCurrentUser(currentUserUsername).money} CLP`);
+$("#currentBalance").text(`$ ${userList[currentUserUsername].money} CLP`);
